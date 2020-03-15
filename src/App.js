7647b188ed8c84350b0f2e4106aa0343688e94d5
Ruditya <rudityap20@gmail.com>
2020-03-16 00:01:09 +0700

    File awal

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import { Card } from 'antd';
+import { Button, Radio, Tooltip } from 'antd';
+import { DownloadOutlined, SearchOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+// function Card( props ){
+//     return(
+//     <div class="card">
+//     <p class="title">{props.name}</p>
+//     <p>{props.job}</p>
+//   </div>
+//       )
+//   }
+//   const App = ()=> {
+//     return (
+//       <div>
+//     <Card name="Prawito Hudoro" job="FrontEnd Dev"/>
+//     <Card name="Mila Ikrima" job="Projects Manager"/>
+//     <Card name="Ki Joko Bodo" job="UI/UX Designer"/>
+//     <Card name="Ruditya Candra" job="BackEnd Developer"/>
+//         </div>
+//     )
+//   }
+//   export default App;
+
+  
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class App    extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <PieChartOutlined />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <DesktopOutlined />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <UserOutlined />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <TeamOutlined />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <FileOutlined />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <Card title="Card title" bordered={false} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+export default App;
+// ReactDOM.render(<SiderDemo />);
